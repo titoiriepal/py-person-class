a49de3c3eb94380f14d670bf7727529a53ca9120
titoiriepal <titoiriepal@hotmail.com>
2026-01-13 13:00:24 +0100

    Suggestions of run flake 8 added

--- app/main.py
+++ app/main.py
@@ -1,19 +1,22 @@
 class Person:
-    
+
     people = {}
-    
-    def __init__(self, name: str, age: int):
+
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[name] = self
-        
-    def __repr__(self):
-        return str(self.__dict__)
-    
+
+
+
 def create_person_list(people: list) -> list:
     person_list = [Person(person["name"], person["age"]) for person in people]
     for i, person in enumerate(people):
         for relation in ("wife", "husband"):
             if person.get(relation):
-                setattr(person_list[i], relation, Person.people[person[relation]])     
+                setattr(
+                    person_list[i],
+                    relation,
+                    Person.people[person[relation]]
+                )
     return person_list
